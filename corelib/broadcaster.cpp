07efa49ae14a7f4ad07b010e7aa3c5c0e325a40b
m1 <m1@gmail.cpm>
2024-07-08 21:39:09 +0300

    removed multiple sendEvent to set<sockaddr>

--- corelib/broadcaster.cpp
+++ corelib/broadcaster.cpp
@@ -17,25 +17,6 @@ void Broadcaster::sendEvent(const msockaddr_in& addr, const SERVICE_id& svs, con
     __BRDifa->sendEvent(addr,svs,e);
     XPASS;
 }
-void Broadcaster::sendEvent(const std::set<msockaddr_in>& addr, const SERVICE_id& svs, const REF_getter<Event::Base>&e)
-{
-    XTRY;
-    if(addr.size()>1)
-    {
-        logErr2("event dump: %s",e->dump().toStyledString().c_str());
-        for(auto& z:addr)
-        {
-            logErr2("addr %s",z.dump().c_str());
-        }
-        logErr2("FATAL: must remove void Broadcaster::sendEvent(const std::set<msockaddr_in>& addr, const SERVICE_id& svs, const REF_getter<Event::Base>&e)");
-    }
-    for(auto &i:addr)
-    {
-
-        __BRDifa->sendEvent(i,svs,e);
-    }
-    XPASS;
-}
 void Broadcaster::sendEvent(const std::string& addr, const SERVICE_id& svs, const REF_getter<Event::Base>&e)
 {
     XTRY;
