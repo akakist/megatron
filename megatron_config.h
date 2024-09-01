0be693ad5569ff979e01f0cdceba56e35aa0b416
m1 <m1@gmail.cpm>
2024-09-01 14:26:33 +0300

    installation into /opt/megatron

--- megatron_config.h
+++ megatron_config.h
@@ -1,13 +1,13 @@
 #ifndef ___megatron_config_h_in
 #define ___megatron_config_h_in
 
-#define PLUGIN_TARGET_DIR "~/bin-10100/plugin"
+#define PLUGIN_TARGET_DIR "/opt/megatron/bin/plugin"
 
-#define DATA_TARGET_DIR "~/bin-10100/data"
-#define CONFIG_TARGET_DIR "~/bin-10100/conf"
-#define CACHE_TARGET_DIR "~/bin-10100/cache"
+#define DATA_TARGET_DIR "/opt/megatron/bin/data"
+#define CONFIG_TARGET_DIR "/opt/megatron/bin/conf"
+#define CACHE_TARGET_DIR "/opt/megatron/bin/cache"
 
-#define LOG_TARGET_DIR "~/bin-10100/log"
+#define LOG_TARGET_DIR "/opt/megatron/bin/log"
 #define CAPS_ADDRESS "x.testquant.com:10100"
 #define CAPS_PORT "10100"
 #define CAPS_IP "x.testquant.com"
