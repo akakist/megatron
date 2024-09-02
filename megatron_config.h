52eac675867285ab94d47918e08bc6fa25d347e9
Alpine <you@alpine.com>
2024-09-02 18:18:22 +0000

    fix cmake

--- megatron_config.h
+++ megatron_config.h
@@ -1,13 +1,13 @@
 #ifndef ___megatron_config_h_in
 #define ___megatron_config_h_in
 
-#define PLUGIN_TARGET_DIR "/opt/megatron/bin/plugin"
+#define PLUGIN_TARGET_DIR "/opt/wallex/plugin"
 
-#define DATA_TARGET_DIR "/opt/megatron/bin/data"
-#define CONFIG_TARGET_DIR "/opt/megatron/bin/conf"
-#define CACHE_TARGET_DIR "/opt/megatron/bin/cache"
+#define DATA_TARGET_DIR "/opt/wallex/data"
+#define CONFIG_TARGET_DIR "/opt/wallex/conf"
+#define CACHE_TARGET_DIR "/opt/wallex/cache"
 
-#define LOG_TARGET_DIR "/opt/megatron/bin/log"
+#define LOG_TARGET_DIR "/opt/wallex/log"
 #define CAPS_ADDRESS "caps.host:10100"
 #define CAPS_PORT "10100"
 #define CAPS_IP "caps.host"
